7baabae563ff1909b699d8aa5c3c9b9f6213f275
MarcoChavez <marco1102004@gmail.com>
2024-03-26 14:52:42 -0600

    añadiendo login

--- src/app/Views/auth/login/login.component.ts
+++ src/app/Views/auth/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,    
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    constructor(
+        private router:Router
+    ){}
+    email: string = ''
+    password: string = ''
 
+
+    register(){
+        this.router.navigate(['/register'])
+    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './Layouts/dashboard/dashboard.component';
 import { HomeComponent } from './Views/Dashboard/home/home.component';
 import { DetalleComponent } from './Components/Sections/Sensores/detalle/detalle.component';
 import { LoginComponent } from './Views/auth/login/login.component';
+import { RegisterComponent } from './Views/auth/register/register.component';
 
 export const routes: Routes = [
     {
@@ -13,6 +14,10 @@ export const routes: Routes = [
             {
                 path: '',
                 component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
             }
         ]
     }
