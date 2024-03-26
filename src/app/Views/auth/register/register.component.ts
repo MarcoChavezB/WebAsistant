08969b31e25006288d52512aa46c57e852b475ec
MarcoChavez <marco1102004@gmail.com>
2024-03-26 15:03:55 -0600

    añadiendo register

--- src/app/Views/auth/register/register.component.ts
+++ src/app/Views/auth/register/register.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+    constructor(
+    private router : Router
+    ){}
+    name : string = ''
+    email: string = ''
+    password: string = ''
+    confirmPassword: string = ''
+    login(){
+        this.router.navigate([''])
+    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './Layouts/dashboard/dashboard.component';
-import { HomeComponent } from './Views/Dashboard/home/home.component';
-import { DetalleComponent } from './Components/Sections/Sensores/detalle/detalle.component';
 import { LoginComponent } from './Views/auth/login/login.component';
 import { RegisterComponent } from './Views/auth/register/register.component';
 
