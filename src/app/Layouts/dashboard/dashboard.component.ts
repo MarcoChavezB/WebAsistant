ba1b4d1c1cc693f60d1cfb3d8db8161e8a35b891
MarcoChavez <marco1102004@gmail.com>
2024-03-25 20:46:21 -0600

    añadiendo asidebar

--- src/app/Layouts/dashboard/dashboard.component.ts
+++ src/app/Layouts/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AsideNavbarComponent } from '../../Components/Sections/aside-navbar/aside-navbar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    AsideNavbarComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
